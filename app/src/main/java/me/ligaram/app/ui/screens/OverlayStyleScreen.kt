6778641b56d0354b26d7370a36e0fe9112ef0b6c
Nuno Neves <nuno.f.neves@gmail.com>
2026-03-08 22:03:38 +0000

    Alteração do número de apresentação do estilo

--- app/src/main/java/me/ligaram/app/ui/screens/OverlayStyleScreen.kt
+++ app/src/main/java/me/ligaram/app/ui/screens/OverlayStyleScreen.kt
@@ -55,7 +55,7 @@ import me.ligaram.app.ui.theme.NavyDeep
 import me.ligaram.app.ui.theme.RiskHigh
 
 // Dados fictícios para o preview
-private const val PREVIEW_NUMBER      = "912 345 678"
+private const val PREVIEW_NUMBER      = "987 654 321"
 private const val PREVIEW_RISK        = "Risco Alto"
 private const val PREVIEW_CATEGORY    = "Fraude"
 private const val PREVIEW_SUBCATEGORY = "Falso investimento"
