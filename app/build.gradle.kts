499ae393ba3b130a7fad82af4391222f23dc7a47
Nuno Neves <nuno.f.neves@gmail.com>
2026-03-15 10:47:26 +0000

    1.0.0-beta.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "me.ligaram.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0-beta.1"
+        versionCode = 2
+        versionName = "1.0.0-beta.2"
     }
 
     buildTypes {
